261ff213975d5a0f4fe567eaac42cdde9d0fed66
Stepan-a11y <kostko52@gmail.com>
2022-05-03 11:30:34 +0300

    Первая выполненная задача в пятом ДЗ

--- HW4_task_1.js
+++
@@ -1,7 +0,0 @@
-function createDebounceFunction(callback, timeDelay) {
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => { callback.apply(this, args); }, timeDelay);
-    };
-} 
